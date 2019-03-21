213cf27377544d182815d13c0168ade95454aab5
Qiang <qzeng2490@yahoo.com>
2019-03-21 18:24:31 +0800

    添加Spring Cloud Bus

--- config-client/src/main/java/qzeng2490/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/qzeng2490/configclient/ConfigClientApplication.java
@@ -2,8 +2,12 @@ package qzeng2490.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
 public class ConfigClientApplication {
 
   public static void main(String[] args) {
